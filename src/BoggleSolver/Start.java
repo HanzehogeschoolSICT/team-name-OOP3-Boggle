8fe527eb1b3bb403db91da9665b924d8bbb5555e
Peter Schriever <peterzen72@gmail.com>
2017-03-15 00:23:27 +0100

    some bug fixes and code fix up, I think it is functioning now

--- src/BoggleSolver/Start.java
+++ src/BoggleSolver/Start.java
@@ -5,10 +5,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by peterzen on 2017-03-13.
@@ -19,12 +16,14 @@ import java.util.Random;
  * - edited variable names for clarification and educational purposes
  * - refactored DictNode class to use a Map for broader character support
  * - refactored some syntax forms
+ * - refactored isInBoard method so it checks if the first character actually exists in the board
  * With the help of http://exceptional-code.blogspot.nl/2012/02/solving-boggle-game-recursion-prefix.html
  */
 public class Start {
     private static final int boardSize = 4;
     private static char[][] boggleBoard = new char[boardSize][boardSize];
     private static DictNode dictRoot;
+    private List<String> foundWords = new LinkedList<>();
 
     private static class DictNode {
         public final char letter;
@@ -82,25 +81,19 @@ public class Start {
         new Start();
     }
 
-    public Start() throws IOException, URISyntaxException {
-        readDict("dict.txt");
-        fillStaticBoggleBoard();
-        long start = System.currentTimeMillis();
-
-        findBoggleWords(dictRoot, new char[50], 0);
-    }
-
     private void findBoggleWords(DictNode node, char[] currentSearch, int currentDepth) {
         if (node == null)
-            return; // a null-node means we have ended on the bottom of the DictNode Tri @TODO: true?
+            return; // a null-node means we have ended on the bottom of the DictNode Tri
 
         if (node.wordEnd && currentDepth > 3) { // we have found a legit word
             // create the word String from our currentSearch characters
-            String word = new String(currentSearch, 0, currentDepth - 1);
+            String word = new String(currentSearch, 0, currentDepth);
             if (isInBoard(word)) {
-                System.out.println(word);
+                foundWords.add(word);
             }
         }
+
+        // @TODO: "super" is skipped? But "supe" is included, actually last char isnt included properly
 
         // delve a level deeper into the dictionary by adding the nextNodes (next characters)
         // and recursively calling this function with an incremented depth
@@ -108,15 +101,6 @@ public class Start {
             currentSearch[currentDepth] = character;
             findBoggleWords(nextNode, currentSearch, currentDepth + 1);
         });
-
-        // @TODO: see if my version does not break ^
-//        for (int i = 0; i < node.nextNodes.length; i++) {
-//            if (node.nextNodes[i] != null) {
-// fill currentBranch[currentHeight] with the char of the current node
-//                currentBranch[currentHeight] = (char) (i + 'a');
-//                boggleTrieDynamic(node.nextNodes[i], currentBranch, currentHeight + 1);
-//            }
-//        }
     }
 
     private boolean isInBoard(final String searchWord) {
@@ -125,29 +109,39 @@ public class Start {
         int[] moveY = {0, 1, 1, 1, 0, -1, -1, -1};
 
         char[] letters = searchWord.toCharArray();
-        boolean[][][] lettersFound = new boolean[letters.length][boardSize][boardSize]; //@TODO: might cause crash (arrSize 50?)
+        boolean[][][] lettersFound = new boolean[letters.length][boardSize][boardSize];
 
-        for (int letterIndex = 0; letterIndex < letters.length; letterIndex++) {
+        // find first character
+        boolean firstFound = false;
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                if (boggleBoard[i][j] == letters[0]) {
+                    lettersFound[0][i][j] = true;
+                    firstFound = true;
+                }
+            }
+        }
+        if (!firstFound)
+            return false;
+
+        for (int letterIndex = 1; letterIndex < letters.length; letterIndex++) {
             // loop through all letters of the searchWord
             for (int i = 0; i < boardSize; i++) {
                 for (int j = 0; j < boardSize; j++) {
                     // loop through all tiles of the boggleBoard
-                    if (letterIndex == 0) {
-                        // always set tested == true for letter 0, position: i,j (because we start at the correct letter)
-                        lettersFound[letterIndex][i][j] = true;
-                    } else {
-                        for (int direction = 0; direction < 8; direction++) {
-                            int x = i + moveX[direction];
-                            int y = j + moveY[direction];
+                    for (int direction = 0; direction < 8; direction++) {
+                        // loop through directions
+                        int x = i + moveX[direction];
+                        int y = j + moveY[direction];
 
-                            // positions are in bounds for board && previous letter was found && character is valid
-                            if (isInBounds(x, y)
-                                    && (lettersFound[letterIndex - 1][x][y])
-                                    && (boggleBoard[i][j] == letters[letterIndex])) {
-                                lettersFound[letterIndex][i][j] = true;
-                                if (letterIndex == letters.length - 1) {
-                                    return true; // we finished the word
-                                }
+                        // positions are in bounds for board && previous letter was found && character is valid
+                        if (isInBounds(x, y)
+                                && (lettersFound[letterIndex - 1][x][y])
+                                && (boggleBoard[i][j] == letters[letterIndex])) {
+                            lettersFound[letterIndex][i][j] = true;
+                            if (letterIndex == letters.length - 1) {
+//                                System.out.println("word: " + searchWord + ", last letter: " + letters[letterIndex]); //@TODO: debug
+                                return true; // we finished the word
                             }
                         }
                     }
@@ -197,5 +191,24 @@ public class Start {
             }
             System.out.println();
         }
+    }
+
+    public Start() throws IOException, URISyntaxException {
+        readDict("dict.txt");
+        fillStaticBoggleBoard();
+        long start = System.currentTimeMillis();
+
+        findBoggleWords(dictRoot, new char[50], 0);
+
+        long end = System.currentTimeMillis();
+        foundWords.forEach(System.out::println); // print all found words
+        System.out.println("Total time spent = " + (end - start) + " milli seconds.");
+
+        printBoggleBoard();
+        System.out.println("Words found: " + foundWords.size());
+
+        // assert that both actually contain the word (should both be true for the static boggleBoard
+        System.out.println(dictRoot.contains("super"));
+        System.out.println(foundWords.contains("super"));
     }
 }
